7c9b745a59c91cb0651f7a02e81d582d07c1fcfd
jumaamrz <jmoooo32@gmail.com>
2022-09-01 09:38:36 +0200

    last update

--- landing.php
+++ landing.php
@@ -57,8 +57,7 @@
 
 	echo "<br/>";
 	// la sous-catégorie est choisie, je peux afficher les éléments concernés
-	if (isset($_POST['recup_id_subcat'])) 
-	{
+	if (isset($_POST['recup_id_subcat'])) {
 		$id_subcat = $_POST['recup_id_subcat'];
 		echo "<div class='d-flex justify-content-center'>";
 		echo "<h5 class='me-3'>";
@@ -79,119 +78,134 @@
 		$query3 = $pdo->query($requete3);
 		$resultat3 = $query3->fetchAll();
 		//afficher le résultat dans un tableau
-		?>
-		<div class="container">	
+	?>
+		<div class="container">
 			<div class="row row-cols-4 justify-content-center">
-		<?php
-			foreach ($resultat3 as $key3 => $variable3) {
-				$category_res = $resultat3[$key3]['nom_category'];
-				$subcategory_res = $resultat3[$key3]['nom_subcategory'];
-				$photo = $resultat3[$key3]['photo_objet'];
-				$id_objet=$resultat3[$key3]['id_objet'];
-				$quantite=$resultat3[$key3]['quantite'];
-				echo "<div class='col mb-3'>";
-				echo "<div class='card ' style='width: 18rem;'>";
-				echo "<img src='assets/images/$category_res/$subcategory_res/$photo.jpg' alt='$photo.jpg' class='img-fluid border-bottom border-dark'>";
-				echo "<div class='card-body'>";
-				echo "<h5 class='card-title'>";
-				echo $resultat3[$key3]['model_objet'];
-				echo "</h5>";
-				
-				// gestion de l'affichage de la quantité et de la disponibilité
-				if ($quantite==0){echo "<br/> Non disponible <br/>";}
-				
-				if ($quantite>0)
-				{
-					if ($quantite==1)
-					{$pluriel='';}
-					if ($quantite>1)
-					{$pluriel='s';}
-					
-				echo "<br/> $quantite disponible$pluriel <br/>";
-				echo "<a href='single.php?id_objet=$id_objet' class='btn btn-secondary mt-1 ms-2'>";
-				echo "Voir la fiche";
-				echo '</a>';	
+				<?php
+				foreach ($resultat3 as $key3 => $variable3) {
+					$category_res = $resultat3[$key3]['nom_category'];
+					$subcategory_res = $resultat3[$key3]['nom_subcategory'];
+					$photo = $resultat3[$key3]['photo_objet'];
+					$id_objet = $resultat3[$key3]['id_objet'];
+					$quantite = $resultat3[$key3]['quantite'];
+					echo "<div class='col mb-3'>";
+					echo "<div class='card ' style='width: 18rem;'>";
+					echo "<img src='assets/images/$category_res/$subcategory_res/$photo.jpg' alt='$photo.jpg' class='img-fluid border-bottom border-dark'>";
+					echo "<div class='card-body'>";
+					echo "<h5 class='card-title'>";
+					echo $resultat3[$key3]['model_objet'];
+					echo "</h5>";
+
+					// gestion de l'affichage de la quantité et de la disponibilité
+					if ($quantite == 0) {
+						echo "<br/> Non disponible <br/>";
+					}
+
+					if ($quantite > 0) {
+						if ($quantite == 1) {
+							$pluriel = '';
+						}
+						if ($quantite > 1) {
+							$pluriel = 's';
+						}
+
+						echo "<br/> $quantite disponible$pluriel <br/>";
+						echo "<a href='single.php?id_objet=$id_objet' class='btn btn-secondary mt-1 ms-2'>";
+						echo "Voir la fiche";
+						echo '</a>';
+					}
+					// fin gestion quantité
+
+
+					echo "</div>";
+					echo "</div>";
+					echo "</div>";
 				}
-				// fin gestion quantité
-				
-				
+			} else {
+
+				echo "<div class='d-flex justify-content-center'>";
+				echo "<h5 class='me-3'>";
+				echo "<p class = 'badge bg-warning text-dark rounded-pill'>";
+				echo "</p>";
+				echo '</h5>';
+				echo "<h5>";
+				echo "<p class = 'badge bg-info text-dark text-start rounded-pill'>";
+				echo "</p>";
+				echo '</h5>';
 				echo "</div>";
-				echo "</div>";
-				echo "</div>";
-			}
-	}
-	
-	else
-	{
-		
-		echo "<div class='d-flex justify-content-center'>";
-		echo "<h5 class='me-3'>";
-		echo "<p class = 'badge bg-warning text-dark rounded-pill'>";
-		echo "</p>";
-		echo '</h5>';
-		echo "<h5>";
-		echo "<p class = 'badge bg-info text-dark text-start rounded-pill'>";
-		echo "</p>";
-		echo '</h5>';
-		echo "</div>";
-		$requete4 = "SELECT * FROM objets,category,sub_category where objets.id_category_objet=category.id_category AND objets.id_subcategory=sub_category.id_subcategory;";
-		//requête pour tester la connexion
-		$query4 = $pdo->query($requete4);
-		$resultat4 = $query4->fetchAll();
-		//afficher le résultat dans un tableau
-		?>
-		<div class="container">	
-			<div class="row row-cols-4 justify-content-center">
-		<?php
-			foreach ($resultat4 as $key4 => $variable4) {
-				$category_res = $resultat4[$key4]['nom_category'];
-				$subcategory_res = $resultat4[$key4]['nom_subcategory'];
-				$photo = $resultat4[$key4]['photo_objet'];
-				$id_objet=$resultat4[$key4]['id_objet'];
-				$quantite=$resultat4[$key4]['quantite'];
-				echo "<div class='col mb-3'>";
-				echo "<div class='card ' style='width: 18rem;'>";
-				echo "<img src='assets/images/$category_res/$subcategory_res/$photo.jpg' alt='$photo.jpg' class='img-fluid border-bottom border-dark'>";
-				echo "<div class='card-body'>";
-				echo "<h5 class='card-title'>";
-				echo $resultat4[$key4]['model_objet'];
-				echo "</h5>";
-								
-				// gestion du calcul , l'affichage de la quantité et de la disponibilité
-				// la quantité est fixe dans la fiche produits,je déduis le nombre de 
-				// produits empruntés pour connaître la quantité réelle dispo en stock
-				
-				//calcul de la quantité réelle
-				$requete_compte = "select * from objets,emprunts where objets.id_objet=$id_objet AND objets.id_objet=emprunts.id_objet;";
-				$query5 = $pdo->query($requete_compte);
-				$resultat5 = $query5->fetchAll();
-				$count=$query5->rowCount();
-				$quantitereelle=$quantite-$count;
-							
-				if ($quantitereelle==0){echo "<br/> Non disponible <br/>";}
-				
-				if ($quantitereelle>0)
-				{
-					if ($quantitereelle==1)
-					{$pluriel='';}
-					if ($quantitereelle>1)
-					{$pluriel='s';}
-					
-				echo "<br/>$quantitereelle disponible$pluriel <br/>";
-				echo "<a href='single.php?id_objet=$id_objet' class='btn btn-secondary mt-1 ms-2'>";
-				echo "Voir la fiche";
-				echo '</a>';	
+				$requete4 = "SELECT * FROM objets,category,sub_category where objets.id_category_objet=category.id_category AND objets.id_subcategory=sub_category.id_subcategory;";
+				//requête pour tester la connexion
+				$query4 = $pdo->query($requete4);
+				$resultat4 = $query4->fetchAll();
+				//afficher le résultat dans un tableau
+				?>
+				<div class="container">
+					<div class="row row-cols-4 justify-content-center">
+					<?php
+					foreach ($resultat4 as $key4 => $variable4) {
+						$category_res = $resultat4[$key4]['nom_category'];
+						$subcategory_res = $resultat4[$key4]['nom_subcategory'];
+						$photo = $resultat4[$key4]['photo_objet'];
+						$id_objet = $resultat4[$key4]['id_objet'];
+						$quantite = $resultat4[$key4]['quantite'];
+						echo "<div class='col mb-3'>";
+						echo "<div class='card ' style='width: 18rem;'>";
+						echo "<img src='assets/images/$category_res/$subcategory_res/$photo.jpg' alt='$photo.jpg' class='img-fluid border-bottom border-dark'>";
+						echo "<div class='card-body'>";
+						echo "<h5 class='card-title'>";
+						echo $resultat4[$key4]['model_objet'];
+						echo "</h5>";
+
+						// gestion du calcul , l'affichage de la quantité et de la disponibilité
+						// la quantité est fixe dans la fiche produits,je déduis le nombre de 
+						// produits empruntés pour connaître la quantité réelle dispo en stock
+
+						//calcul de la quantité réelle
+						$requete_compte = "select * from objets,emprunts where objets.id_objet=$id_objet AND objets.id_objet=emprunts.id_objet;";
+						$query5 = $pdo->query($requete_compte);
+						$resultat5 = $query5->fetchAll();
+						$count = $query5->rowCount();
+						$quantitereelle = $quantite - $count;
+
+						if ($quantitereelle == 0) {
+							echo "<div class='d-flex justify-content-center align-items-center'>";
+							echo "<span class='dot me-2'>";
+							echo "</span>";
+							echo "Non disponibe";
+							echo "</div>";
+							echo "<button disabled href='single.php?id_objet=$id_objet' class='btn btn-secondary mt-2 d-block w-50 m-auto'>";
+							echo "Voir la fiche";
+							echo '</button>';
+						}
+
+						if ($quantitereelle > 0) {
+							if ($quantitereelle == 1) {
+								$pluriel = '';
+							}
+							if ($quantitereelle > 1) {
+								$pluriel = 's';
+							}
+
+							echo "<div class='d-flex justify-content-center align-items-center'>";
+							echo "<span class='dot-1 me-2'>";
+							echo "</span>";
+
+							echo "$quantitereelle disponible$pluriel";
+							echo "</div>";
+							echo "<a href='single.php?id_objet=$id_objet' class='btn btn-dark mt-2 d-block w-50 m-auto'>";
+							echo "Voir la fiche";
+							echo '</a>';
+						}
+						// fin gestion quantité
+
+
+						echo "</div>";
+						echo "</div>";
+						echo "</div>";
+					}
 				}
-				// fin gestion quantité
-				
-				
-				echo "</div>";
-				echo "</div>";
-				echo "</div>";
-			}
-	}
-		?>
-		</div>
-	</div>
-</div>
-<?php include './inc/footer.php' ?>
+					?>
+					</div>
+				</div>
+			</div>
+			<?php include './inc/footer.php' ?>
