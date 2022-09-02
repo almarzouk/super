a4ed5eac0558b415fa0008a16b3f22f550dc9866
patlablate <103847030+patlablate@users.noreply.github.com>
2022-09-02 08:39:22 +0200

    Add files via upload
    error - this is the last navbar  !

--- navbar.php
+++ navbar.php
@@ -1,28 +1,93 @@
+<?php
+
+
+
+$rep_default = "/super/";
+$url_standard = "http://" . $_SERVER['HTTP_HOST'] . $rep_default;
+// echo $url_standard;
+
+?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="../index.php">Super</a>
+        <a class="navbar-brand" href="<?php echo $url_standard ?>index.php">Super</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- condition-->
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="#">list des objets</a>
+						
+               
+			<?php
+	
+					if((isset($_SESSION['auth']) && $_SESSION['auth']==0))
+			{ 
+		?> 
+
+					
+        <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>landing.php">Liste des objets</a>
+                </li>
+		
+			 <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>emprunts.php?option_page=0">Historique des emprunts</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="#">Historique des objets</a>
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard; ?>emprunts.php?option_page=1">les emprunts en cours</a>
+                </li>
+				
+			<?php 
+			}
+
+			
+			// liens administrateur 
+			
+			if(isset($_SESSION['auth']) &&($_SESSION['auth']==1))
+			{ 
+		?> 
+
+					
+                <li class="nav-item">
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>admin/insert-objet.php">insert objet</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  me-4" aria-current="page" href="#">les emprunt en cours</a>
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>admin/insert-user.php">insert user</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  me-4" aria-current="page" href="./admin/insert-objet.php">insert objet</a>
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>admin/update-objet.php">update objet</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link  me-4" aria-current="page" href="../admin/insert-user.php">insert user</a>
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>admin/update-user.php">update user</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>admin/delete-objet.php">delete objet</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>admin/delete-user.php">delete user</a>
+                </li>
+				
+			<?php 
+			}
+
+			?>
             </ul>
         </div>
     </div>
-</nav>
+	
+
+<div style="color:white;font-weight:bolder">
+	<?php
+	
+	//affichage des données de session dans un encart dédié
+	if (isset($_SESSION['nom_user']))
+	{
+	echo $_SESSION['nom_user']." connecté<br />";
+	echo "<a href='$url_standard/inc/session_deconnect.php'>Se déconnecter</a>";
+	}
+	else
+	{
+	echo "non connecté";
+	}
+	?>
+	</div>
+	</nav>
