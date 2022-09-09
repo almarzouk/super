8b0bd0179c4197585c4ca67e67a6f3ccfa12f291
patlablate <103847030+patlablate@users.noreply.github.com>
2022-09-09 11:01:35 +0200

    Add files via upload
    modify logo link

--- inc/navbar.php
+++ inc/navbar.php
@@ -4,7 +4,7 @@ include 'functions.php';
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="<?php echo $url_standard ?>/index.php">
-            <img src="./assets/images/logo-c.png" alt="logo" style="max-width: 80px;">
+            <img src="<?php echo $url_standard; ?>/assets/images/logo-c.png" alt="logo" style="max-width: 80px;">
         </a>
         <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
