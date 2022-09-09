12c484bca0155ce5a49a24fcdd9056dfeddde1a4
patlablate <103847030+patlablate@users.noreply.github.com>
2022-09-09 10:16:55 +0200

    Add files via upload
    upload with creat thumb ok

--- inc/connection.php
+++ inc/connection.php
@@ -4,7 +4,7 @@ $hote = 'localhost';
 $db = 'emprunt';
 $port = '3306';
 $charset = 'utf8';
-$user = 'jumaa';
+$user = 'root';
 $password = '';
 
 $dsn = "mysql:$hote=localhost;dbname=$db;port=$port;charset=$charset";
@@ -14,7 +14,10 @@ $dsn = "mysql:$hote=localhost;dbname=$db;port=$port;charset=$charset";
 // création et test de la connexion
 try {
 	$pdo = new PDO($dsn, "$user", "$password");
-} catch (PDOException $exception) {
+} catch (PDOException $exception) 
+{
+	// créer la bdd avec import des tables
+	echo "<a>Créer la Bdd et les tables du projet dans MySQL</a>";
 	// on peut par exemple envoyer un mail mail('monmail@detest','PDOException' $exception -> getMessage());
 	exit('Erreur de connexion à la base de données');
 }
--- inc/navbar.php
+++ inc/navbar.php
@@ -1,35 +1,31 @@
 <?php
 include 'functions.php';
 ?>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mh-50" style="min-height: 70px;">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo $url_standard ?>/index.php">
-            <img src="./assets/images/logo-c.png" alt="logo" style="max-width: 80px;">
-        </a>
-        <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- condition-->
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-
+        <a class="navbar-brand" href="<?php echo $url_standard ?>/landing.php"></a>
+        <div class="collapse navbar-collapse mt-5 mt-lg-0" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
                 <?php
-
                 if ((isset($_SESSION['auth']) && $_SESSION['auth'] == 0)) {
                 ?>
-
-
+                    <!-- condition-->
                     <li class="nav-item">
                         <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>/landing.php">Liste des objets</a>
                     </li>
 
                     <li class="nav-item">
+                        <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>/emprunts.php?option_page=1">les emprunts en cours</a>
+                    </li>
+
+                    <li class="nav-item">
                         <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>/emprunts.php?option_page=0">Historique des emprunts</a>
                     </li>
+
                     <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard; ?>/emprunts.php?option_page=1">les emprunts en cours</a>
+                        <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>/admin/update-user.php">Modifier mes données personnelles</a>
                     </li>
+
 
                 <?php
                 }
@@ -41,25 +37,33 @@ include 'functions.php';
                 ?>
 
                     <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/landing.php">Dashboard </a>
+                        <a class="nav-link " aria-current="page" href="<?php echo $url_standard ?>/landing.php">Dashboard </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Action
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/insert-objet.php">Ajouter un objet </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/insert-user.php">Ajouter un utilisateur</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/list-objet.php">Modifier/Supprimer un objet</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/list-user.php">Modifier/Supprimer Données Utilisateur</a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/insert-objet.php">Ajouter un objet </a>
+                        <a class="nav-link" aria-current="page" href="<?php echo $url_standard; ?>/emprunts.php?option_page=1">les emprunts en cours</a>
                     </li>
+
                     <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/insert-user.php">Ajouter un utilisateur</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/update-objet.php">update objet</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/update-user.php">update user</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/delete-objet.php">Supprimer un objet</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link  me-4" aria-current="page" href="<?php echo $url_standard ?>/admin/delete-user.php">Supprimer un utilisateur</a>
+                        <a class="nav-link " aria-current="page" href="<?php echo $url_standard; ?>/emprunts.php?option_page=0">Historique des emprunts</a>
                     </li>
 
                 <?php
@@ -67,20 +71,21 @@ include 'functions.php';
 
                 ?>
             </ul>
-        </div>
-        <div style="color:white;font-weight:bolder">
-            <?php
+            <div class='text-white d-flex justify-content-start align-items-start me-3'>
+                <?php
 
-            //affichage des données de session dans un encart dédié
-            if (isset($_SESSION['nom_user'])) {
-                echo $_SESSION['nom_user'] . " connecté<br />";
-                echo "<a href='$url_standard/inc/session_deconnect.php'>Se déconnecter</a>";
-            } else {
-                echo "non connecté";
-            }
-            ?>
+                //affichage des données de session dans un encart dédié
+                if (isset($_SESSION['nom_user'])) {
+                    echo $_SESSION['nom_user'] . "<p class='me-2 ms-2 p-0 m-0'>connecté</p>";
+                    echo "<a class='btn btn-danger p-2' href='$url_standard/inc/session_deconnect.php' style='padding-block: 0 !important;'>x</a>";
+                } else {
+                    echo "non connecté";
+                }
+                ?>
+            </div>
         </div>
+        <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     </div>
-
-
 </nav>
